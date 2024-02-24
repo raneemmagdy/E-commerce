d2ede5e8f954460acb1c389439cf4513e658ccaa
raneemmagdy <raneemmagdy2002@gmail.com>
2024-02-24 19:21:17 +0200

    Updates

---
+++ static/js/896.89876c05.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkecommerceapp=self.webpackChunkecommerceapp||[]).push([[896],{8896:(e,r,c)=>{c.r(r),c.d(r,{default:()=>s});c(9060);var p=c(2496);function s(){return(0,p.jsx)("div",{children:"Orders"})}}}]);
+//# sourceMappingURL=896.89876c05.chunk.js.map
